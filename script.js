403e9546d2a2902b65d2d778e5f5f2b7ff0a820f
dehsetdefnis <wunderlanddaphne@gmail.com>
2026-05-26 16:42:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -125,7 +125,7 @@ async function startStory() {
 
   await new Promise(r => setTimeout(r, 2500));
 
-  await showChapter("BÖLÜM 1");
+  await showChapter("bölüm 1");
 }
 
 // ✅ FIXED CHAPTER SYSTEM (ARTIK GARANTİ GÖRÜNÜR)
