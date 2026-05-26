4891bc94c0729665cc618b97ed78e529e215576d
dehsetdefnis <wunderlanddaphne@gmail.com>
2026-05-26 17:07:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ async function startStory() {
 
   await new Promise(r => setTimeout(r, 2500));
 
-  await showChapter("BÖLÜM 1");
+  await showChapter("bölüm 1");
 }
 
 // 🔥 FINAL CINEMATIC CHAPTER SYSTEM
